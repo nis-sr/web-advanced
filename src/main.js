18632e2edbaef12928fa8eccc6d9ccfeabe14987
nis-sr <nisrine.assarar@student.ehb.be>
2025-05-17 14:59:34 +0200

    structuur+ zoekfunctionaliteit
    html:
    H1 herwerkt
    -zoekbalk gewijzigd en zoekknop -toegevoegd
    -vervanging in main
    JS:
    -searchinput,serachbutton en resultcontainer geselecteerd met getelementbyid

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
+const resultsContainer=document.getElementById("results");
+
+searchButton.addEventListener("click",()=>{
+    const query=searchInput.ariaValueMax.trim();
+    if(!query){
+        alert("typ een zoekterm in!");
+    }else{
+        console.log("zoeken naar",query);
+        
+    }
+});
 
 
